849e5ab80c0684731a113c78811e616ec44c8bfd
LastTribunal <thebigsky@gmail.com>
2022-06-23 21:52:20 -0400

    +

--- app.js
+++ app.js
@@ -5,34 +5,32 @@ const Piscina = require('piscina');
 
 const piscina = new Piscina({
     filename: path.resolve(__dirname, "worker.js"),
-    maxThreads: 2
+    maxThreads: 10
 });
 
 (async () => {
-    const thread1 =  piscina.run({
-        a: 4,
-        b: 6,
-        ms: 1000
-    });
-    const thread2 =  piscina.run({
-        a: 4,
-        b: 54,
-        ms: 1000
-    });
-    const thread3 =  piscina.run({
-        a: 4,
-        b: 666,
-        ms: 1000
-    });
-
-   var allThreads =[];
-   allThreads.push(thread1);
-   allThreads.push(thread2);
-   allThreads.push(thread3); 
-
-   var start= Date.now();
-   var result = await Promise.all(allThreads);
    
-   console.log(result);
-   console.log(`execution time: ${Date.now()-start}`);
+   
+    var allThreads = [];
+    for (let index = 0; index< 10; index++) {
+
+       
+        var thread = piscina.run({
+            ms: getRandomInt(10000)
+        });
+        allThreads.push(thread);
+
+    }
+
+    function getRandomInt(max) {
+        return Math.floor(Math.random() * max);
+    }
+
+
+
+    var start = Date.now();
+    var result = await Promise.all(allThreads);
+
+    console.log(result);
+    console.log(`execution time: ${Date.now()-start}`);
 })();
--- worker.js
+++ worker.js
@@ -1,10 +1,10 @@
 /*jshint esversion: 9 */
-const { setTimeout } = require('timers/promises');
+const {
+  setTimeout
+} = require('timers/promises');
 module.exports = async ({
-  a,
-  b,
   ms
 }) => {
-  await setTimeout(ms, 'resolved');
-  return a + b;
+  await setTimeout(ms);
+  return ms;
 };
